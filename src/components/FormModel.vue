ce679e77d2d66250ff8c2eb6c30afdb47e5e3cfb
Saqaf <saqafaliabdo716@gmail.com>
2025-08-05 10:32:07 +0300

    add form model

---
+++ src/components/FormModel.vue
@@ -0,0 +1,52 @@
+<template>
+  <form class="mt-6">
+    <div class="mb-4">
+      <label
+        for="Image"
+        class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
+        >Image</label
+      >
+      <input
+        type="file"
+        class="block w-full px-4 py-2 mt-2 text-red-700 bg-white border rounded-md focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600"
+      />
+    </div>
+    <div class="mb-2">
+      <label
+        for="Alt"
+        class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
+        >Alt</label
+      >
+      <input
+        type="text"
+        class="block w-full px-4 py-2 mt-2 text-red-700 bg-white border rounded-md focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600"
+      />
+    </div>
+    <div class="mb-2">
+      <label
+        for="Figcaption"
+        class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
+        >Figcaption</label
+      >
+<textarea placeholder="Enter  figcaption image here...
+" name="message" rows="4" cols="50" class="block w-full px-4 py-2 mt-2 text-red-700 bg-white border rounded-md focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600">
+</textarea>
+
+
+      
+ 
+    </div>
+
+    <div class="mt-6">
+      <button
+        class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600"
+      >
+        Login
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
--- src/components/notifications/Model.vue
+++ src/components/notifications/Model.vue
@@ -1,10 +1,12 @@
-<template>
+  <template>
   <i title="Add"  @click="showModal" class="fa-solid fa-plus text-gray-500 cursor-pointer"> </i>
   <!-- <button class="btn" @click="showModal">open modal</button> -->
   <dialog id="myModel" class="modal modal-bottom sm:modal-middle">
-    <div class="modal-box">
+    <div class="modal-box bg-gradient-to-br from-[#936a66] via-[#3db8b1]/80 to-[#180b09] border border-cyan-400/40 " >
       <h3 class="text-lg font-bold">Hello!</h3>
-      <p class="py-4">Press ESC key or click the button below to close</p>
+       <div class="relative flex flex-col items-center justify-center ">
+          <FormModel /></div>
+
       <div class="modal-action">
         <form method="dialog">
           <!-- if there is a button in form, it will close the modal -->
@@ -15,7 +17,12 @@
   </dialog>
 </template>
 <script>
+import FormModel from '../FormModel.vue';
+
 export default {
+  components:{
+    FormModel,
+  },
   methods: {
     showModal() {
       document.getElementById("myModel").showModal();
